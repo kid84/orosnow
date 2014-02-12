b440bebe030279b8f9eca95c5821fc8404640068
masamitsu-konya <masamitsu.konya@gmail.com>
2014-02-12 12:46:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orosnow::Application.config.secret_key_base = 'f1243d4f85040934a160692c79c082aaa901025adc9242ac8d5cb3eebbe570f794ab503caaf7205ef7a6ea2eb48c09514e723a896d0eaace2a65f6abcd3b56e3'
