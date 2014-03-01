57125241d315bb3b85d0c3b617dcb091e96fee71
rtoya <toya@ig-partners.co.jp>
2014-03-02 04:01:10 +0900

    フォーム修正（タイミングとpassword以外）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  require 'geocoder'
   def index
   end
 
@@ -8,14 +9,15 @@ class UsersController < ApplicationController
 
   def registration
     @user = User.new
-    @user.address = params[:address]
-    @user.mail = params[:mail]
+    user = params[:user]
+    @user.address = user[:address]
+    @user.mail = user[:mail]
     @user.password = 00000000 #prams[:password]
     @user.salt = 00000000 #password暗号化の処理
     @user.timing_id = 30 #params[:timing_id]
-    @user.total_depth = 30 #params[:total_depth]
-    @user.latitude = 35.65 #@user.convert_address_to_lat
-    @user.longitude = 139.736 #@user.convert_address_to_lng
+    @user.total_depth = user[:total_depth]
+    @user.latitude = @user.convert_address_to_lat
+    @user.longitude = @user.convert_address_to_lng
 
     if @user.save!
       render :text => "success"
